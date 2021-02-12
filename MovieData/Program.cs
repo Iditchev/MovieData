5907510b6343c96003f542d7f9320bbc868bbb69
iditchev <iditchev@my.wctc.edu>
2021-02-11 20:17:28 -0600

    Adding some exception handling and starting user adding movie

--- MovieData/Program.cs
+++ MovieData/Program.cs
@@ -17,6 +17,8 @@ namespace MovieData
 
             if (resp == "1")
             {
+                try
+                {
                 StreamReader sr = new StreamReader("movies.csv");
                 while (!sr.EndOfStream)
                 {
@@ -44,12 +46,35 @@ namespace MovieData
 
                     Console.WriteLine();
                 }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("File could not be found or opened");
+                }
+                
+                
                
             }
             else if (resp == "2")
             {
-
-                // read data from file
+                int result;
+                do 
+                {
+                    
+                    Console.WriteLine("Enter 1 to add a movie");
+                    Console.WriteLine("Enter 0 to stop");
+                    bool input = Int32.TryParse(Console.ReadLine(), out result);
+                    if (input)
+                    {
+                        Console.WriteLine("Please enter movie title");
+                        string movieTitle = Console.ReadLine();
+                        Console.WriteLine("Please enter the year the movie was released");
+                        string movieYear = Console.ReadLine();
+                        Console.WriteLine("Please enter movie Genre(s), seperate each with a comma if there are multiple");
+                        string movieGenres = Console.ReadLine();
+                        string[] movieGenresArr = movieGenres.Split(',');
+                    }
+                } while(result == 0 );
 
 
             }
